9e91859701b5218d97060832fbf67f5be9399e06
Anuj kumar sharma <141046905+anuj308@users.noreply.github.com>
2025-02-18 21:56:21 +0530

    env setup errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    OPENAI_API_BASE_URL: process.env.OPENAI_API_BASE_URL,
+  },
 };
-
-export default nextConfig;
